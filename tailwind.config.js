80c04a34e701f19a3797ea76747e2a5928b9ad24
evandrofaria <evandrofaria@outlook.com>
2024-03-10 19:52:47 -0300

    feat(*): wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,ts}"],
+    content: [
+        "./src/**/*.{html,ts}",
+        // "./node_modules/flowbite/**/*.js"
+    ],
     theme: {
         extend: {
             colors: {
                 'primary': 'var(--primary-color)',
                 'secondary-color': 'var(--secondary-color)',
-                'primary-shade': 'var(--ion-color-primary-shade)',
-                'primary-tint': 'var(--ion-color-primary-tint)',
                 'secondary-text': 'var(--secondary-text)',
+                'state-error': 'var(--state-error)',
+                'state-warning': 'var(--state-warning)',
+                'state-success': 'var(--state-success)',
                 'gradient-gray': 'var(--gradient-gray)',
                 'card-color': 'var(--card-color)'
             },
@@ -16,26 +20,29 @@ module.exports = {
                 'default': 'var(--ion-background-color)',
                 'alternative': 'var(--alternative-bg)'
             },
-            // screens: {
-            //     mobile: {min: '0px', max: '768px'},
-            //     desktop: '769px'
-            // },
             fontFamily: {
                 sans: ['"Plus Jakarta Sans", sans-serif',]
+            },
+            screens: {
+                mobile: {min: '0px', max: '768px'},
+                desktop: '769px'
             }
         },
     },
-    // plugins: [require("daisyui")],
-    // daisyui: {
-    //     themes: [
-    //         {
-    //             mytheme: {
-    //                 "primary": "#FFFFFF",
-    //                 "secondary": "#5BADFF",
-    //                 "base-100": "#2A2A2A",
-    //             },
-    //         }
-    //     ]
-    // }
+    plugins: [require('flowbite/plugin'), require("daisyui")],
+    daisyui: {
+        prefix: {
+            prefix: "daisy-",
+            themes: [
+                    {
+                        mytheme: {
+                            "primary": "#FFFFFF",
+                            "secondary": "#5BADFF",
+                            "base-100": "#2A2A2A",
+                        },
+                    }
+                ]
+        }
+    }
 }
 
